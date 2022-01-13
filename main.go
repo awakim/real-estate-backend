80921298f41ec1985765f949cce53249081a5f20
awakim <arnold.wakim@gmail.com>
2022-01-13 15:32:09 +0100

    add viper to load config from file or env.

--- main.go
+++ main.go
@@ -6,17 +6,16 @@ import (
 
 	"github.com/awakim/immoblock-backend/api"
 	db "github.com/awakim/immoblock-backend/db/sqlc"
+	"github.com/awakim/immoblock-backend/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/immoblock?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
@@ -24,7 +23,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
