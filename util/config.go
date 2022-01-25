a25f382695f3fc9e1eb32505425793732aa7fcd4
awakim <arnold.wakim@gmail.com>
2022-01-25 21:52:35 +0100

    adding cors to middleware

--- util/config.go
+++ util/config.go
@@ -17,6 +17,7 @@ type Config struct {
 	TokenSymmetricKey    string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
 	AccessTokenDuration  time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
 	RefreshTokenDuration time.Duration `mapstructure:"REFRESH_TOKEN_DURATION"`
+	CorsOrigin           string        `mapstructure:"CORS_ORIGIN"`
 }
 
 // LoadConfig reads configuration from file or environment variables.
